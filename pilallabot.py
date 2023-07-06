6ef2a0d835110ea91ef77771ae42de4037023fee
Mikko Rouru <mikko.rouru@gmail.com>
2023-07-06 12:16:11 +0300

    Added finnishnouns.txt, pre-commit hooks and refactored the code

--- pilallabot.py
+++ pilallabot.py
@@ -1,49 +1,53 @@
-import tweepy, sys, random, pickle
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import tweepy
+import random
+import pickle
 
 try:
-	#Twitter credentials
-	CONSUMER_KEY = 'YOUR_CONSUMER_KEY'
-	CONSUMER_SECRET = 'YOUR_CONSUMER_SECRET'
-	ACCESS_KEY = 'YOUR_ACCESS_KEY'
-	ACCESS_SECRET = 'YOUR_ACCESS_SECRET'
+    # Twitter credentials
+    CONSUMER_KEY: str = "YOUR_CONSUMER_KEY"
+    CONSUMER_SECRET: str = "YOUR_CONSUMER_SECRET"
+    ACCESS_KEY: str = "YOUR_ACCESS_KEY"
+    ACCESS_SECRET: str = "YOUR_ACCESS_SECRET"
 
-	#Setting up the authentication and API for Twitter
-	auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-	auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-	api = tweepy.API(auth)
+    # Setting up the authentication and API for Twitter
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    api = tweepy.API(auth)
 
-	def main():
-		#If using cron job, the path must be placed manually
-		numberOfNouns = sum(1 for line in open('finnishnouns.txt', 'r'))
-			
-		#Create a pickle file for maintaining already used nouns
-		pickle_file = open('nounfile.pickle', 'ab')
-		try:
-			usedNouns = pickle.load(open('nounfile.pickle', 'rb'))
-		except EOFError:
-			usedNouns = []
+    def main():
+        # If using cron job, the path must be placed manually
+        number_of_nouns: int = sum(1 for line in open("finnishnouns.txt", "r"))
 
-		#Get a random noun
-		nounIndex = random.randint(0, numberOfNouns-1)
-		while nounIndex in usedNouns:
-			nounIndex = random.randint(0, numberOfNouns-1)
-		
-		#If using cron job, the path must be placed manually
-		noun = open('finnishnouns.txt', 'r').readlines()[nounIndex].strip()
+        # Create a pickle file for maintaining already used nouns
+        pickle_file = open("nounfile.pickle", "ab")
+        try:
+            used_nouns: list = pickle.load(open("nounfile.pickle", "rb"))
+        except EOFError:
+            used_nouns: list = []
 
-		#Check if the noun has already been tweeted
-		if len(usedNouns) < numberOfNouns:
-			usedNouns.append(nounIndex)
-			with open ('nounfile.pickle', 'wb') as pickle_file:
-				pickle.dump(usedNouns, pickle_file)
+        # Get a random noun
+        noun_index: int = random.randint(0, number_of_nouns - 1)
+        while noun_index in used_nouns:
+            noun_index = random.randint(0, number_of_nouns - 1)
 
-		#Form the tweet
-		tweet = noun + " on pilalla"
-			
-		api.update_status(tweet)
+        # If using cron job, the path must be placed manually
+        noun: str = open("finnishnouns.txt", "r").readlines()[noun_index].strip()
 
-	if __name__ == "__main__":
-		main()
-		
+        # Check if the noun has already been tweeted
+        if len(used_nouns) < number_of_nouns:
+            used_nouns.append(noun_index)
+            with open("nounfile.pickle", "wb") as pickle_file:
+                pickle.dump(used_nouns, pickle_file)
+
+        # Form the tweet
+        tweet: str = noun + " on pilalla"
+
+        api.update_status(tweet)
+
+    if __name__ == "__main__":
+        main()
+
 except Exception as e:
     print(e)
